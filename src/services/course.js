293041439ca97ddb77e78ac1bfe24593c37045a6
Ivan Snehith <ivansnehith23@gmail.com>
2023-02-26 23:45:55 +0530

    Added withcredntials

--- src/services/course.js
+++ src/services/course.js
@@ -6,23 +6,29 @@ const ENDPOINT_BASE = "courses/create_course";
 export const createCourse = (payload) => {
   const url = `${API_BASE_URL}/${ENDPOINT_BASE}/landing_page`;
 
-  return axios.post(url, payload, { maxBodyLength: Infinity });
+  return axios.post(url, payload, {
+    maxBodyLength: Infinity,
+    withCredentials: true,
+  });
 };
 
 export const createCourseMoudle = (payload) => {
   const url = `${API_BASE_URL}/${ENDPOINT_BASE}/module`;
 
-  return axios.post(url, payload);
+  return axios.post(url, payload, { withCredentials: true });
 };
 
 export const createMoudleLecture = (payload) => {
   const url = `${API_BASE_URL}/${ENDPOINT_BASE}/lecture`;
 
-  return axios.post(url, payload);
+  return axios.post(url, payload, { withCredentials: true });
 };
 
 export const addLectureResource = (payload) => {
   const url = `${API_BASE_URL}/${ENDPOINT_BASE}/lecture/resource`;
 
-  return axios.post(url, payload, { maxBodyLength: Infinity });
+  return axios.post(url, payload, {
+    maxBodyLength: Infinity,
+    withCredentials: true,
+  });
 };
